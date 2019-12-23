9c8f99043bf3a69a61605168abbab00a1e5ce6e8
thanhnguyenhu66 <55022095+thanhnguyenhu66@users.noreply.github.com>
2019-12-23 15:04:59 +0000

    Bullet design 1st attempt done but unable to test

---
+++ core/src/com/septagon/entites/Bullet.java
@@ -0,0 +1,36 @@
+package com.septagon.entites;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Bullet {
+
+    public static final int SPEED = 500;
+    public static Texture texture;
+    public static final int DEFAULT_Y = 40;
+
+    float x, y;
+    public boolean remove = false;
+
+    public Bullet (float x) {
+        this.x = x;
+        this.y = DEFAULT_Y;
+
+        if (texture == null) {
+            texture = new Texture("bullet.png");
+        }
+    }
+
+    public void update (float deltaTime) {
+        y+= SPEED * deltaTime;
+        if (y > Gdx.graphics.getHeight())
+            remove = true;
+    }
+
+    public void render (SpriteBatch batch){
+        batch.draw(texture, x, y);
+    }
+}
+
+
